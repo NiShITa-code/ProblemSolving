6fe1702c8b52408e968ff77bdba99b973ef0c2dc
NiShITa-code <75923914+NiShITa-code@users.noreply.github.com>
2023-06-29 07:54:23 +0530

    Added solution - LeetHub

---
+++ Next Happy Number - GFG/next-happy-number.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    bool happyNum(int N) {
+        if (N == 1 || N == 7) {
+            return true;
+        }
+        // false for 2,3,4,5,6,8,9
+        if (N >= 2 && N <= 9) {
+            return false;
+        }
+        int squareSum = 0;
+        while (N) {
+            int x = N % 10;
+            squareSum += x*x;
+            N/= 10;
+        }
+        
+        return happyNum(squareSum);
+    }
+    int nextHappy(int N){
+        // code here
+        while(true) {
+            N++;
+            if (happyNum(N)) {
+                return N;
+            }
+        }
+        
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.nextHappy(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
