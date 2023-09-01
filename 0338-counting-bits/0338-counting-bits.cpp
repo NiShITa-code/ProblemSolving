40ed593596ea2897b98939a2e38b1c7f26ff6c84
Nishita Jain <75923914+NiShITa-code@users.noreply.github.com>
2023-09-01 08:56:08 +0530

    Time: 6 ms (56.68%), Space: 7.8 MB (65.8%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int noOfOnes(int i) {
-        int setBits = 0;
-        while (i > 0) {
-            i = i & (i - 1);
-            setBits++;
-        }
-        return setBits;
-    }
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1);
-        for (int i = 0; i <= n; i++) {
-            ans[i] = noOfOnes(i);
-        }
-        return ans;
+        //TC = O(N)
+        // n+1 as we are going to count from 0 to n
+        vector<int> t(n+1);
+        
+        // t[0] will be 0 beacuse 0 has count of set bit is 0;
+        t[0] = 0;
+        
+        // we can compute current set bit count using previous count
+        // as x/2 in O(1) time
+        
+        // i%2 will be 0 for even number ans 1 for odd number
+        
+        for(int i = 1; i<=n; ++i)
+            t[i] = t[i/2] + i%2;
+        
+        return t;
     }
 };
+        
+    
