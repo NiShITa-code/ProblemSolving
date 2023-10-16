d69617b4d18ffd273774d85fc4fc3213c59e032f
Nishita Jain <75923914+NiShITa-code@users.noreply.github.com>
2023-10-16 07:48:54 +0530

    Time: 0 ms (100%), Space: 6.6 MB (63.89%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res;
+        long long ans = 1;
+        res.push_back(ans);
+        for (int i = 1; i <= rowIndex; i++) {
+            ans = ans * (rowIndex + 1 - i);
+            ans = ans / i;
+            res.push_back(ans);
+        }
+        return res;
+    
+    }
+};
