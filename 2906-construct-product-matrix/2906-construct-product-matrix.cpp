a732eb65c473a4b43f781e3a4bcadbf074625f3f
Nishita Jain <75923914+NiShITa-code@users.noreply.github.com>
2026-03-24 00:41:58 +0000

    Time: 22 ms (95.57%), Space: 132 MB (80.07%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
-        const int MOD = 12345;
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int>> ans(n, vector<int>(m, 1));
-
-        // Prefix product
-        long long prefix = 1;
-        for (int idx = 0; idx < n * m; idx++) {
-            int r = idx / m;
-            int c = idx % m;
-
-            ans[r][c] = prefix;
-            prefix = (prefix * (grid[r][c] % MOD)) % MOD;
-        }
-
-        // Suffix product
-        long long suffix = 1;
-        for (int idx = n * m - 1; idx >= 0; idx--) {
-            int r = idx / m;
-            int c = idx % m;
-
-            ans[r][c] = (ans[r][c] * suffix) % MOD;
-            suffix = (suffix * (grid[r][c] % MOD)) % MOD;
-        }
-
+    static vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        const int N=12345, n=grid.size(), m=grid[0].size();
+        vector<vector<int>> ans(n, vector<int>(m));
+        uint64_t pref=1, suf=1;
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++){
+                ans[i][j]=pref;
+                pref=pref*grid[i][j]%N;
+            }
+        for(int i=n-1; i>=0; i--)
+            for(int j=m-1; j>=0; j--){
+                ans[i][j]=ans[i][j]*suf%N;
+                suf=suf*grid[i][j]%N;
+            }
         return ans;
     }
 };
