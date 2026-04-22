15ad30c8bd0d08530c1549f843fd49cd6116a7ad
Nishita Jain <75923914+NiShITa-code@users.noreply.github.com>
2026-04-22 01:24:42 +0100

    Time: 0 ms (100%), Space: 12.5 MB (88.24%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-     
-    bool check(string& q, vector<string>& dict){
-        for(auto d: dict){
-            int c = 0;
-            for(int i = 0; i < d.size(); ++i){
-                if(q[i] != d[i]) c++;
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> result;
+        for(auto &q : queries){
+            for(auto &d : dictionary){
+                int count = 0;
+                for(int i = 0; i < q.length(); i++){
+                    if(q[i] != d[i]){
+                        count++;
+                    }
+                    if(count > 2){
+                        break;
+                    }
+                }
+                if(count <= 2){
+                    result.push_back(q);
+                    break;
+                }
             }
-            if(c <= 2) return true;
         }
-        return false;
-    }
-    vector<string> twoEditWords(vector<string>& queries, vector<string>& dict) {
-        vector<string> ans;
-        for(auto q: queries){
-            if(check(q, dict)) ans.push_back(q);
-        }
-        return ans;
+
+        return result;
     }
 };
