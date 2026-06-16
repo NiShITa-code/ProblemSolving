cb15b53e18f78c9b7ffac4a20332e1de64cb587e
Nishita Jain <75923914+NiShITa-code@users.noreply.github.com>
2026-06-16 01:19:30 +0100

    Time: 0 ms (100%), Space: 312.1 MB (54.89%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -1,29 +1,19 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if (head == NULL || head -> next == NULL) {
-            return nullptr;
-        }
+        if (!head->next) return nullptr;
+
         ListNode* slow = head;
-        // we want to stop slow pointer one step behind the middle element
-        ListNode* fast = head -> next -> next;
-        while (fast != NULL && fast -> next != NULL) {
-            slow = slow -> next;
-            fast = fast -> next -> next;
+        ListNode* fast = slow->next->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode* temp = slow -> next;
-        slow -> next = slow -> next -> next;
-        delete(temp);
+
+        slow->next = slow->next->next;
         return head;
     }
 };
+
+
